f96f19f1ad3773e29980941cd19bf02ffdb3097a
Isadora Miranda <isalala188@gmail.com>
2026-04-10 18:57:46 -0300

    Update user insert statements with hashed passwords

--- init.sql
+++ init.sql
@@ -16,5 +16,5 @@ CREATE TABLE IF NOT EXISTS orders (
     status VARCHAR(20) DEFAULT 'Aberto'
 );
 
-INSERT INTO users (username, password) VALUES ('admin', 'admin123');
-INSERT INTO items (name, category) VALUES ('Arroz Branco', 'Base'), ('Feijão Preto', 'Grão');
+INSERT INTO users (username, password) VALUES ('admin', '$2b$10$EPf9X7A8ytYmGvYJIn.S9OphHInR6T6tUfLdfx8A6p.O7n8F9WpS.');
+INSERT INTO users (username, password) VALUES ('isadora', '$2b$10$vM2BfIuYyvW.Z3k9Xq2mOu1T2S0G1zL8a5u7z3mR4xQ9e8a7i6o5u');
